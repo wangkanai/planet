72a7743b037c5028723f18b484afd3d918f8d452
wangkanai <sarin@wangkanai.com>
2025-06-21 14:05:02 +0700

    Add `Coordinate`, `AddressTile`, and `PixelTile` classes to `Spatial` for 2D/3D spatial representations.

---
+++ Spatial/src/Root/PixelTile.cs
@@ -0,0 +1,10 @@
+// Copyright (c) 2014-2025 Sarin Na Wangkanai, All Rights Reserved. Apache License, Version 2.0
+
+namespace Wangkanai.Planet.Spatial;
+
+public class PixelTile
+{
+	public int X { get; set; }
+	public int Y { get; set; }
+	public int Z { get; set; }
+}
