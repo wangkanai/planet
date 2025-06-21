0e2d9eb0076716e2f189222d9228365775946221
wangkanai <sarin@wangkanai.com>
2025-06-21 14:43:41 +0700

    Add console project `Wangkanai.Planet.Engine.Console` with minimal setup and root command configuration

--- Engine/src/Console/Program.cs
+++ Engine/src/Console/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.CommandLine;
+using System.Text;
+
+namespace Wangkanai.Planet.Engine;
+
+public static class Program
+{
+	private static async Task<int> Main(string[] args)
+	{
+		if (System.Console.IsInputRedirected)
+			System.Console.OutputEncoding = Encoding.UTF8;
+
+		var rootCommand = new RootCommand("Wangkanai Planet Engine");
+
+		return await rootCommand.InvokeAsync(args);
+	}
+}
