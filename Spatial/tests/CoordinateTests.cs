4274cd092a6df2aad9da3bbd9f067ab367999d14
wangkanai <sarin@wangkanai.com>
2025-06-21 14:34:13 +0700

    Configure testing setup for `Spatial` and centralize framework versioning

---
+++ Spatial/tests/CoordinateTests.cs
@@ -0,0 +1,22 @@
+// Copyright (c) 2014-2025 Sarin Na Wangkanai, All Rights Reserved. Apache License, Version 2.0
+
+namespace Wangkanai.Planet.Spatial.Tests;
+
+public class CoordinateTests
+{
+	[Fact]
+	public void CoordinatePair_WhenCreatedWithDefaultConstructor_HasZeroValues()
+	{
+		var coordinate = new Coordinate();
+		Assert.Equal(0, coordinate.X);
+		Assert.Equal(0, coordinate.Y);
+	}
+
+	[Fact]
+	public void CoordinatePair_WhenCreatedWithValues_HasCorrectValues()
+	{
+		var coordinate = new Coordinate(1, 2);
+		Assert.Equal(1, coordinate.X);
+		Assert.Equal(2, coordinate.Y);
+	}
+}
