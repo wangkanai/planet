5e67086771bcf99f055a47d2e232835e10bcfb3d
wangkanai <sarin@wangkanai.com>
2025-06-21 08:09:18 +0700

    Add `Theme` enum for theme selection, update `Color` namespace, and configure `RootNamespace` in project file

--- Common/src/Domain/Generic/Color.cs
+++ Common/src/Domain/Generic/Color.cs
@@ -1,6 +1,6 @@
 // Copyright (c) 2014-2025 Sarin Na Wangkanai, All Rights Reserved. Apache License, Version 2.0
 
-namespace Wangkanai.Planet.Common.Domain.Generic;
+namespace Wangkanai.Planet.Common.Generic;
 
 // https://tabler.io/docs/base/colors
 [Flags]
