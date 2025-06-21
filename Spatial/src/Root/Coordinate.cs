65d441ccc7fa62beb7c046b4735bf907610dbc88
wangkanai <sarin@wangkanai.com>
2025-06-21 14:23:59 +0700

    Simplify `Coordinate` XML documentation format

--- Spatial/src/Root/Coordinate.cs
+++ Spatial/src/Root/Coordinate.cs
@@ -2,10 +2,7 @@
 
 namespace Wangkanai.Planet.Spatial;
 
-/// <summary>
-/// Represents a coordinate pair with X and Y values.
-/// Can be used for geographical coordinates, pixel positions, or any 2D coordinate system.
-/// </summary>
+/// <summary>Represents a coordinate pair with X and Y values. Can be used for geographical coordinates, pixel positions, or any 2D coordinate system.</summary>
 public class Coordinate
 {
 	public Coordinate() { }
