d0a0f98fd1bf38bbc946549f0c44370fdecb0760
wangkanai <sarin@wangkanai.com>
2025-06-30 08:35:28 +0700

    Remove unnecessary comment from PlanetUser class

--- Portal/src/Domain/Identity/PlanetUser.cs
+++ Portal/src/Domain/Identity/PlanetUser.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Identity;
 
 namespace Wangkanai.Planet.Portal.Data;
 
-// Add profile data for application users by adding properties to the ApplicationUser class
 public class PlanetUser : IdentityUser
 {
 	public string   Firstname { get; set; }
