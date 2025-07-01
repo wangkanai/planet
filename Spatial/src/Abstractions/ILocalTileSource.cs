a6731761ad65e47814b9bb6a5c4762de7275d08a
wangkanai <sarin@wangkanai.com>
2025-07-01 13:18:52 +0700

    Add MbTiles support with interfaces and enums for tile management

---
+++ Spatial/src/Abstractions/ILocalTileSource.cs
@@ -0,0 +1,12 @@
+// Copyright (c) 2014-2025 Sarin Na Wangkanai, All Rights Reserved. Apache License, Version 2.0
+
+namespace Wangkanai.Planet.Spatial;
+
+public interface ILocalTileSource { }
+
+public interface ITileSource
+{
+	string      Name        { get; }
+	ITileSchema Schema      { get; }
+	Attribution Attribution { get; }
+}
---
+++ Spatial/src/Abstractions/ITileSchema.cs
@@ -0,0 +1,10 @@
+// Copyright (c) 2014-2025 Sarin Na Wangkanai, All Rights Reserved. Apache License, Version 2.0
+
+namespace Wangkanai.Planet.Spatial;
+
+/// <summary>Defines the schema for a tile.</summary>
+public interface ITileSchema
+{
+	/// <summary>Gets the name of the tile schema.</summary>
+	string Name { get; }
+}
