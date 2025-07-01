c72a596683944425e2c73e86996089d078de8cdb
wangkanai <sarin@wangkanai.com>
2025-07-02 00:05:30 +0700

    Refactor PlanetRoles enum to use sequential values for improved clarity and maintainability

--- Portal/src/Domain/Identity/PlanetRoles.cs
+++ Portal/src/Domain/Identity/PlanetRoles.cs
@@ -3,13 +3,12 @@
 namespace Wangkanai.Planet.Portal.Data;
 
 /// <summary>Defines the different roles that a user can have in the system.</summary>
-[Flags]
 public enum PlanetRoles
 {
-	None        = 0,     // 0000_0000
-	Admin       = 1 << 0,// 0000_0001
-	Moderator   = 1 << 1,// 0000_0010
-	Editor      = 1 << 2,// 0000_0100
-	Contributor = 1 << 3,// 0000_1000
-	User        = 1 << 4,// 0001_0000
+	None        = 0,
+	Admin       = 1,
+	Moderator   = 2,
+	Editor      = 3,
+	Contributor = 4,
+	User        = 5,
 }
