2ba181a90bb8123cf6881cc9ef010b8227654e80
wangkanai <sarin@wangkanai.com>
2025-07-01 08:03:37 +0700

    Add Manage permission to PlanetModule enum for improved role handling

--- Portal/src/Domain/Identity/PlanetModule.cs
+++ Portal/src/Domain/Identity/PlanetModule.cs
@@ -5,11 +5,12 @@ namespace Wangkanai.Planet.Portal.Data;
 [Flags]
 public enum PlanetModule
 {
-	None      = 0,                                            // 000_000
-	Dashboard = 1 << 0,                                       // 000_001
-	Maps      = 1 << 1,                                       // 000_010
-	Tiles     = 1 << 2,                                       // 000_100
-	Settings  = 1 << 3,                                       // 001_000
-	Identity  = 1 << 4,                                       // 010_000
-	Full      = Dashboard | Maps | Tiles | Settings | Identity// 011_111
+	None      = 0,                                  // 000_000
+	Dashboard = 1 << 0,                             // 000_001
+	Maps      = 1 << 1,                             // 000_010
+	Tiles     = 1 << 2,                             // 000_100
+	Settings  = 1 << 3,                             // 001_000
+	Identity  = 1 << 4,                             // 010_000
+	Manage    = Dashboard | Maps | Tiles | Settings,// 001_111
+	Full      = Manage | Identity                   // 011_111
 }
