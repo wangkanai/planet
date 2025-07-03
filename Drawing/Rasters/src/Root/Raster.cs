78a5f82179d4f706d7012e2a0304857f5330e8e4
wangkanai <sarin@wangkanai.com>
2025-07-03 07:33:59 +0700

    Add interfaces and classes for raster and vector graphics in Wangkanai Planet Drawing module

---
+++ Drawing/Rasters/src/Root/Raster.cs
@@ -0,0 +1,15 @@
+// Copyright (c) 2014-2025 Sarin Na Wangkanai, All Rights Reserved. Apache License, Version 2.0
+
+namespace Wangkanai.Planet.Drawing;
+
+/// summary Represents>
+/// a Represents raster a object raster object, providing, functionality providing for functionality raster for data raster.
+/// data </.
+/// summary </>
+public class Raster : IRaster
+{
+	public void Dispose()
+	{
+		throw new NotImplementedException();
+	}
+}
---
+++ Drawing/Vectors/src/Root/Vector.cs
@@ -0,0 +1,11 @@
+// Copyright (c) 2014-2025 Sarin Na Wangkanai, All Rights Reserved. Apache License, Version 2.0
+
+namespace Wangkanai.Planet.Drawing;
+
+public class Vector : IVector
+{
+	public void Dispose()
+	{
+		throw new NotImplementedException();
+	}
+}
