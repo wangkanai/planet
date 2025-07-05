5531de5e62e87704a31eb8d0de413c6e4c2a1b1e
wangkanai <sarin@wangkanai.com>
2025-07-05 11:40:50 +0700

    refactor: update references from Drawing to Graphics in GeoTiffRaster classes

--- Spatial/src/GeoTiffs/IGeoTiffRaster.cs
+++ Spatial/src/GeoTiffs/IGeoTiffRaster.cs
@@ -1,6 +1,6 @@
 // Copyright (c) 2014-2025 Sarin Na Wangkanai, All Rights Reserved. Apache License, Version 2.0
 
-using Wangkanai.Planet.Drawing.Rasters.Tiffs;
+using Wangkanai.Graphics.Rasters.Tiffs;
 
 namespace Wangkanai.Planet.Spatial;
 
@@ -9,19 +9,19 @@ public interface IGeoTiffRaster : ITiffRaster
 {
 	/// <summary>Gets the coordinate reference system (CRS) of the geospatial data.</summary>
 	string? CoordinateReferenceSystem { get; }
-	
+
 	/// <summary>Gets the geographic extent of the image.</summary>
 	MapExtent? Extent { get; }
-	
+
 	/// <summary>Gets the transformation matrix for converting pixel coordinates to geographic coordinates.</summary>
 	double[]? GeoTransform { get; }
-	
+
 	/// <summary>Gets a value indicating whether the image contains geospatial metadata.</summary>
 	bool IsGeoreferenced { get; }
-	
+
 	/// <summary>Gets the pixel size in geographic units.</summary>
 	double? PixelSizeX { get; }
-	
+
 	/// <summary>Gets the pixel size in geographic units.</summary>
 	double? PixelSizeY { get; }
 }
