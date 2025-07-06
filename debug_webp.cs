a15622b8f78fdbe8f4b381a1b43cdbbde6100d08
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2025-07-06 16:00:27 +0700

    delate debug

--- debug_webp.cs
+++
@@ -1,7 +0,0 @@
-using Wangkanai.Graphics.Rasters.Webps;
-
-var webp = new WebPRaster();
-Console.WriteLine($"Initial: Format={webp.Format}, Compression={webp.Compression}");
-
-webp.Compression = WebPCompression.VP8L;
-Console.WriteLine($"After setting VP8L: Format={webp.Format}, Compression={webp.Compression}");
