259257a9de1cd4e68d5364ac79ac21159bed7f17
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2025-07-06 15:03:00 +0700

    Add WebP raster image support with interface and implementation

---
+++ Graphics/Rasters/src/Root/Webps/WebPConstants.cs
@@ -0,0 +1,103 @@
+// Copyright (c) 2014-2025 Sarin Na Wangkanai, All Rights Reserved. Apache License, Version 2.0
+
+namespace Wangkanai.Graphics.Rasters.Webps;
+
+/// <summary>Defines constants for WebP format specifications.</summary>
+public static class WebPConstants
+{
+	/// <summary>The WebP signature bytes (RIFF header).</summary>
+	public static readonly byte[] Signature = "RIFF"u8.ToArray();
+
+	/// <summary>The WebP format identifier.</summary>
+	public static readonly byte[] FormatId = "WEBP"u8.ToArray();
+
+	/// <summary>The VP8 chunk identifier for lossy compression.</summary>
+	public static readonly byte[] VP8ChunkId = "VP8 "u8.ToArray();
+
+	/// <summary>The VP8L chunk identifier for lossless compression.</summary>
+	public static readonly byte[] VP8LChunkId = "VP8L"u8.ToArray();
+
+	/// <summary>The VP8X chunk identifier for extended features.</summary>
+	public static readonly byte[] VP8XChunkId = "VP8X"u8.ToArray();
+
+	/// <summary>The ALPH chunk identifier for alpha channel.</summary>
+	public static readonly byte[] AlphaChunkId = "ALPH"u8.ToArray();
+
+	/// <summary>The ANIM chunk identifier for animation.</summary>
+	public static readonly byte[] AnimChunkId = "ANIM"u8.ToArray();
+
+	/// <summary>The ANMF chunk identifier for animation frames.</summary>
+	public static readonly byte[] AnimFrameChunkId = "ANMF"u8.ToArray();
+
+	/// <summary>The ICCP chunk identifier for ICC profile.</summary>
+	public static readonly byte[] IccProfileChunkId = "ICCP"u8.ToArray();
+
+	/// <summary>The EXIF chunk identifier for EXIF metadata.</summary>
+	public static readonly byte[] ExifChunkId = "EXIF"u8.ToArray();
+
+	/// <summary>The XMP chunk identifier for XMP metadata.</summary>
+	public static readonly byte[] XmpChunkId = "XMP "u8.ToArray();
+
+	/// <summary>The minimum width for WebP images.</summary>
+	public const uint MinWidth = 1;
+
+	/// <summary>The maximum width for WebP images.</summary>
+	public const uint MaxWidth = 16383;
+
+	/// <summary>The minimum height for WebP images.</summary>
+	public const uint MinHeight = 1;
+
+	/// <summary>The maximum height for WebP images.</summary>
+	public const uint MaxHeight = 16383;
+
+	/// <summary>The minimum quality level for lossy compression.</summary>
+	public const int MinQuality = 0;
+
+	/// <summary>The maximum quality level for lossy compression.</summary>
+	public const int MaxQuality = 100;
+
+	/// <summary>The default quality level for lossy compression.</summary>
+	public const int DefaultQuality = 75;
+
+	/// <summary>The minimum compression level for lossless compression.</summary>
+	public const int MinCompressionLevel = 0;
+
+	/// <summary>The maximum compression level for lossless compression.</summary>
+	public const int MaxCompressionLevel = 9;
+
+	/// <summary>The default compression level for lossless compression.</summary>
+	public const int DefaultCompressionLevel = 6;
+
+	/// <summary>The size of the RIFF header in bytes.</summary>
+	public const int RiffHeaderSize = 12;
+
+	/// <summary>The size of a chunk header in bytes.</summary>
+	public const int ChunkHeaderSize = 8;
+
+	/// <summary>The size of the VP8X chunk data in bytes.</summary>
+	public const int VP8XChunkSize = 10;
+
+	/// <summary>The size of the ANIM chunk data in bytes.</summary>
+	public const int AnimChunkSize = 6;
+
+	/// <summary>The size of the alpha preprocessing flag in bytes.</summary>
+	public const int AlphaPreprocessingSize = 1;
+
+	/// <summary>The maximum number of animation loops (0 = infinite).</summary>
+	public const ushort MaxAnimationLoops = ushort.MaxValue;
+
+	/// <summary>The default background color for animations (transparent).</summary>
+	public const uint DefaultBackgroundColor = 0x00000000;
+
+	/// <summary>Estimated overhead for WebP container and chunks.</summary>
+	public const int ContainerOverhead = 50;
+
+	/// <summary>The bit depth for WebP images (always 8 bits per channel).</summary>
+	public const byte BitDepth = 8;
+
+	/// <summary>The number of color channels in RGB format.</summary>
+	public const byte RgbChannels = 3;
+
+	/// <summary>The number of color channels in RGBA format.</summary>
+	public const byte RgbaChannels = 4;
+}
---
+++ Graphics/Rasters/src/Root/Webps/WebPPreset.cs
@@ -0,0 +1,25 @@
+// Copyright (c) 2014-2025 Sarin Na Wangkanai, All Rights Reserved. Apache License, Version 2.0
+
+namespace Wangkanai.Graphics.Rasters.Webps;
+
+/// <summary>Defines the WebP encoding presets for optimization.</summary>
+public enum WebPPreset : byte
+{
+	/// <summary>Default preset with balanced quality and size.</summary>
+	Default = 0,
+
+	/// <summary>Optimized for pictures with lots of detail.</summary>
+	Picture = 1,
+
+	/// <summary>Optimized for photographic images.</summary>
+	Photo = 2,
+
+	/// <summary>Optimized for drawings and graphics.</summary>
+	Drawing = 3,
+
+	/// <summary>Optimized for icon images.</summary>
+	Icon = 4,
+
+	/// <summary>Optimized for text content.</summary>
+	Text = 5
+}
