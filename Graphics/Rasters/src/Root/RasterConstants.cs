9fd7c66062baf1e18c53d503ecfd79c2b5ffe6ec
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2025-07-07 23:06:48 +0700

    feat: Introduce core image and metadata interfaces with organized namespace structure

--- Graphics/Rasters/src/Root/RasterConstants.cs
+++ Graphics/Rasters/src/Root/RasterConstants.cs
@@ -1,6 +1,6 @@
 // Copyright (c) 2014-2025 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
 
-namespace Wangkanai.Graphics.Rasters.Metadatas;
+namespace Wangkanai.Graphics.Rasters;
 
 /// <summary>
 /// Defines common constants used across raster image formats.
