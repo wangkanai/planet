77f7b45f6ed55b2573cd9131d7218ffb31074f0c
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2025-07-07 21:33:56 +0700

    feat: Refactor HeifMetadata class to inherit from RasterMetadataBase and streamline metadata properties

--- Graphics/Rasters/src/Root/Heifs/HeifMetadata.cs
+++ Graphics/Rasters/src/Root/Heifs/HeifMetadata.cs
@@ -2,104 +2,23 @@
 
 namespace Wangkanai.Graphics.Rasters.Heifs;
 
+using System.Threading.Tasks;
 using Wangkanai.Graphics.Rasters.Metadatas;
 
 /// <summary>
 /// Represents comprehensive metadata for HEIF images including EXIF, XMP, ICC profiles, and HDR information.
 /// </summary>
-public sealed class HeifMetadata : IDisposable, IAsyncDisposable
+public sealed class HeifMetadata : RasterMetadataBase
 {
-	private bool _disposed;
-
-	/// <summary>
-	/// Gets or sets the EXIF metadata.
-	/// </summary>
-	public byte[]? ExifData { get; set; }
-
-	/// <summary>
-	/// Gets or sets the XMP metadata.
-	/// </summary>
-	public byte[]? XmpData { get; set; }
-
-	/// <summary>
-	/// Gets or sets the ICC color profile.
-	/// </summary>
-	public byte[]? IccProfile { get; set; }
-
 	/// <summary>
 	/// Gets or sets the HDR metadata.
 	/// </summary>
 	public HdrMetadata? HdrMetadata { get; set; }
 
 	/// <summary>
-	/// Gets or sets the image creation timestamp.
+	/// Gets or sets the camera metadata.
 	/// </summary>
-	public DateTimeOffset? CreationTime { get; set; }
-
-	/// <summary>
-	/// Gets or sets the image modification timestamp.
-	/// </summary>
-	public DateTimeOffset? ModificationTime { get; set; }
-
-	/// <summary>
-	/// Gets or sets the software used to create the image.
-	/// </summary>
-	public string? Software { get; set; }
-
-	/// <summary>
-	/// Gets or sets the image description or caption.
-	/// </summary>
-	public string? Description { get; set; }
-
-	/// <summary>
-	/// Gets or sets the image copyright information.
-	/// </summary>
-	public string? Copyright { get; set; }
-
-	/// <summary>
-	/// Gets or sets the image author or artist.
-	/// </summary>
-	public string? Author { get; set; }
-
-	/// <summary>
-	/// Gets or sets the camera make.
-	/// </summary>
-	public string? CameraMake { get; set; }
-
-	/// <summary>
-	/// Gets or sets the camera model.
-	/// </summary>
-	public string? CameraModel { get; set; }
-
-	/// <summary>
-	/// Gets or sets the lens make.
-	/// </summary>
-	public string? LensMake { get; set; }
-
-	/// <summary>
-	/// Gets or sets the lens model.
-	/// </summary>
-	public string? LensModel { get; set; }
-
-	/// <summary>
-	/// Gets or sets the focal length in millimeters.
-	/// </summary>
-	public double? FocalLength { get; set; }
-
-	/// <summary>
-	/// Gets or sets the aperture (f-number).
-	/// </summary>
-	public double? Aperture { get; set; }
-
-	/// <summary>
-	/// Gets or sets the exposure time in seconds.
-	/// </summary>
-	public double? ExposureTime { get; set; }
-
-	/// <summary>
-	/// Gets or sets the ISO sensitivity.
-	/// </summary>
-	public int? IsoSensitivity { get; set; }
+	public CameraMetadata? CameraMetadata { get; set; }
 
 	/// <summary>
 	/// Gets or sets the GPS coordinates.
@@ -110,11 +29,6 @@ public sealed class HeifMetadata : IDisposable, IAsyncDisposable
 	/// Gets or sets the orientation of the image.
 	/// </summary>
 	public ImageOrientation Orientation { get; set; } = ImageOrientation.Normal;
-
-	/// <summary>
-	/// Gets or sets the pixel density in pixels per inch.
-	/// </summary>
-	public double? PixelDensity { get; set; }
 
 	/// <summary>
 	/// Gets or sets the color space information.
@@ -157,17 +71,14 @@ public sealed class HeifMetadata : IDisposable, IAsyncDisposable
 	public Dictionary<string, byte[]>? AuxiliaryImages { get; set; }
 
 	/// <summary>
-	/// Gets the total estimated size of all metadata in bytes.
+	/// Gets the total estimated memory usage of all metadata in bytes.
 	/// </summary>
-	public long EstimatedSize
+	public override long EstimatedMemoryUsage
 	{
 		get
 		{
-			var size = 0L;
+			var size = base.EstimatedMemoryUsage;
 			
-			if (ExifData != null) size += ExifData.Length;
-			if (XmpData != null) size += XmpData.Length;
-			if (IccProfile != null) size += IccProfile.Length;
 			if (ThumbnailData != null) size += ThumbnailData.Length;
 			if (PreviewData != null) size += PreviewData.Length;
 			if (DepthMapData != null) size += DepthMapData.Length;
@@ -178,8 +89,8 @@ public sealed class HeifMetadata : IDisposable, IAsyncDisposable
 					size += aux.Length;
 			}
 
-			// Add estimated size for text properties and HDR metadata
-			size += 2048;
+			// Add estimated size for HEIF-specific properties
+			size += 1024;
 			
 			return size;
 		}
@@ -188,80 +99,46 @@ public sealed class HeifMetadata : IDisposable, IAsyncDisposable
 	/// <summary>
 	/// Gets whether this metadata contains large data that might benefit from async disposal.
 	/// </summary>
-	public bool HasLargeData => EstimatedSize > HeifConstants.Memory.DefaultMetadataBufferSizeMB * 1024 * 1024;
+	public bool HasLargeData => EstimatedMemoryUsage > HeifConstants.Memory.DefaultMetadataBufferSizeMB * 1024 * 1024;
 
 	/// <summary>
 	/// Creates a copy of this metadata instance.
 	/// </summary>
 	/// <returns>A new metadata instance with copied values.</returns>
-	public HeifMetadata Clone()
+	public override IRasterMetadata Clone()
 	{
-		ThrowIfDisposed();
-
-		return new HeifMetadata
-		{
-			ExifData = ExifData?.ToArray(),
-			XmpData = XmpData?.ToArray(),
-			IccProfile = IccProfile?.ToArray(),
-			HdrMetadata = HdrMetadata?.Clone(),
-			CreationTime = CreationTime,
-			ModificationTime = ModificationTime,
-			Software = Software,
-			Description = Description,
-			Copyright = Copyright,
-			Author = Author,
-			CameraMake = CameraMake,
-			CameraModel = CameraModel,
-			LensMake = LensMake,
-			LensModel = LensModel,
-			FocalLength = FocalLength,
-			Aperture = Aperture,
-			ExposureTime = ExposureTime,
-			IsoSensitivity = IsoSensitivity,
-			GpsCoordinates = GpsCoordinates?.Clone(),
-			Orientation = Orientation,
-			PixelDensity = PixelDensity,
-			ColorSpaceInfo = ColorSpaceInfo,
-			WhiteBalance = WhiteBalance,
-			CodecParameters = CodecParameters != null ? new Dictionary<string, object>(CodecParameters) : null,
-			CustomMetadata = CustomMetadata != null ? new Dictionary<string, object>(CustomMetadata) : null,
-			ThumbnailData = ThumbnailData?.ToArray(),
-			PreviewData = PreviewData?.ToArray(),
-			DepthMapData = DepthMapData?.ToArray(),
-			AuxiliaryImages = AuxiliaryImages?.ToDictionary(
-				kvp => kvp.Key, 
-				kvp => kvp.Value.ToArray())
-		};
+		var clone = new HeifMetadata();
+		CopyBaseTo(clone);
+		
+		clone.HdrMetadata = HdrMetadata?.Clone();
+		clone.CameraMetadata = CameraMetadata?.Clone();
+		clone.GpsCoordinates = GpsCoordinates?.Clone();
+		clone.Orientation = Orientation;
+		clone.ColorSpaceInfo = ColorSpaceInfo;
+		clone.WhiteBalance = WhiteBalance;
+		clone.CodecParameters = CodecParameters != null ? new Dictionary<string, object>(CodecParameters) : null;
+		clone.CustomMetadata = CustomMetadata != null ? new Dictionary<string, object>(CustomMetadata) : null;
+		clone.ThumbnailData = ThumbnailData?.ToArray();
+		clone.PreviewData = PreviewData?.ToArray();
+		clone.DepthMapData = DepthMapData?.ToArray();
+		clone.AuxiliaryImages = AuxiliaryImages?.ToDictionary(
+			kvp => kvp.Key, 
+			kvp => kvp.Value.ToArray());
+			
+		return clone;
 	}
 
 	/// <summary>
 	/// Clears all metadata.
 	/// </summary>
-	public void Clear()
+	public override void Clear()
 	{
-		ThrowIfDisposed();
-
-		ExifData = null;
-		XmpData = null;
-		IccProfile = null;
+		base.Clear();
+		
 		HdrMetadata = null;
-		CreationTime = null;
-		ModificationTime = null;
-		Software = null;
-		Description = null;
-		Copyright = null;
-		Author = null;
-		CameraMake = null;
-		CameraModel = null;
-		LensMake = null;
-		LensModel = null;
-		FocalLength = null;
-		Aperture = null;
-		ExposureTime = null;
-		IsoSensitivity = null;
+		CameraMetadata = null;
 		GpsCoordinates = null;
 		Orientation = ImageOrientation.Normal;
-		PixelDensity = null;
 		ColorSpaceInfo = null;
 		WhiteBalance = null;
 		CodecParameters?.Clear();
@@ -272,64 +149,36 @@ public sealed class HeifMetadata : IDisposable, IAsyncDisposable
 		AuxiliaryImages?.Clear();
 	}
 
-	private void ThrowIfDisposed()
+	/// <inheritdoc />
+	protected override void Dispose(bool disposing)
 	{
-		if (_disposed)
-			throw new ObjectDisposedException(nameof(HeifMetadata));
+		if (disposing)
+		{
+			HdrMetadata = null;
+			CameraMetadata = null;
+			ThumbnailData = null;
+			PreviewData = null;
+			DepthMapData = null;
+			CodecParameters?.Clear();
+			CustomMetadata?.Clear();
+			AuxiliaryImages?.Clear();
+		}
+		
+		base.Dispose(disposing);
 	}
 
 	/// <inheritdoc />
-	public void Dispose()
+	public override async ValueTask DisposeAsync()
 	{
-		if (_disposed)
-			return;
-
-		ExifData = null;
-		XmpData = null;
-		IccProfile = null;
-		HdrMetadata = null;
-		ThumbnailData = null;
-		PreviewData = null;
-		DepthMapData = null;
-		CodecParameters?.Clear();
-		CustomMetadata?.Clear();
-		AuxiliaryImages?.Clear();
-
-		_disposed = true;
-	}
-
-	/// <inheritdoc />
-	public ValueTask DisposeAsync()
-	{
-		if (_disposed)
-			return ValueTask.CompletedTask;
-
 		// For large metadata, we might want to do async cleanup
 		if (HasLargeData)
 		{
-			return DisposeAsyncCore();
+			await Task.Run(() => Dispose(true)).ConfigureAwait(false);
 		}
-
-		Dispose();
-		return ValueTask.CompletedTask;
-	}
-
-	private async ValueTask DisposeAsyncCore()
-	{
-		// Simulate async cleanup for large metadata
-		await Task.Yield();
-		
-		ExifData = null;
-		XmpData = null;
-		IccProfile = null;
-		HdrMetadata = null;
-		ThumbnailData = null;
-		PreviewData = null;
-		DepthMapData = null;
-		CodecParameters?.Clear();
-		CustomMetadata?.Clear();
-		AuxiliaryImages?.Clear();
-
-		_disposed = true;
+		else
+		{
+			Dispose(true);
+		}
+		GC.SuppressFinalize(this);
 	}
 }
