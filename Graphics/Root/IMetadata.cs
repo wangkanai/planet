5882c6f135cc222e10e0ad53ef7a0e7fe4e53efa
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2025-07-07 22:57:15 +0700

    feat: Refactor metadata interfaces for improved organization and consistency
    Reference issue #98

---
+++ Graphics/Root/IMetadata.cs
@@ -0,0 +1,5 @@
+// Copyright (c) 2014-2025 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
+namespace Wangkanai.Graphics;
+
+public interface IMetadata : IDisposable, IAsyncDisposable { }
